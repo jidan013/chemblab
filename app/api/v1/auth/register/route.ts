9feff21cb36960d7d812a13af16772abd3678a78
jidan013 <jiddan.mhs@narotama.ac.id>
2026-03-06 21:00:53 +0700

    update

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -3,7 +3,6 @@ import { hashPassword, requireRoleOrNull } from "@/lib/auth";
 import db from "@/lib/db";
 import { registerSchema } from "@/lib/validation/auth";
 import { NextRequest, NextResponse } from "next/server";
-import type { Prisma } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
   try {
@@ -71,7 +70,7 @@ export async function POST(request: NextRequest) {
 
     const hashedPassword = await hashPassword(password);
 
-    const newUser = await db.$transaction(async (tx: Prisma.TransactionClient) => {
+    const newUser = await db.$transaction(async (tx) => {
       const user = await tx.user.create({
         data: {
           email,
